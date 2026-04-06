f65c4e0f38181f89aaee9b76dd6a925484a07f6a
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-06 11:13:11 -0400

    add mnist parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use mnist::{Mnist, MnistBuilder};
+use ndarray::prelude::{Array3, s};
+
 fn main() {
-    println!("Hello, world!");
+    let Mnist {
+        trn_img,
+        // trn_lbl,
+        // tst_img,
+        // tst_lbl,
+        ..
+    } = MnistBuilder::new()
+        .training_images_filename("archive/train-images.idx3-ubyte")
+        .training_labels_filename("archive/train-labels.idx1-ubyte")
+        .test_images_filename("archive/t10k-images.idx3-ubyte")
+        .test_labels_filename("archive/t10k-labels.idx1-ubyte")
+        .label_format_digit()
+        .training_set_length(50_000)
+        .test_set_length(10_000)
+        .finalize();
+
+   let train_data = Array3::from_shape_vec((50_000, 28, 28), trn_img)
+       .expect("Error converting images to Array3 struct");
+   println!("{:#?}\n", train_data.slice(s![0, .., ..]));
 }
