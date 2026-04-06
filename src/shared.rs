ca9fdca922924b05cecfb6445c8c65633c56abbe
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-06 10:31:20 -0400

    add basic stuff

---
+++ src/shared.rs
@@ -0,0 +1,21 @@
+use priority_queue::PriorityQueue;
+
+struct Image {
+    name: String,
+}
+
+fn calculate_distance_between_pixels(px1: i8, px2: i8) {
+    return (px1 - px2).abs();
+}
+
+fn calculate_distance_between_images(img1: &Image, img2: &Image) -> i8 {
+
+}
+
+fn find_closest_images(k: i32, pq: &PriorityQueue) {
+
+}
+
+fn predict_image_category(image: &Image) {
+
+}
