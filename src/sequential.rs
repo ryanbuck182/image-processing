e71acaf5945a4d722c8b432de531e8779cb93d6d
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-10 01:22:01 -0400

    reorder functions in sequential.rs

--- src/sequential.rs
+++ src/sequential.rs
@@ -1,24 +1,6 @@
 use priority_queue::PriorityQueue;
 use shared::{Image, calculate_distance_between_images};
 
-pub fn find_closest_images(k: u32, image: &Image, train_images: Vec<Image>) -> Vec<u8> {
-    let pq = PriorityQueue::with_capacity(k as usize);
-
-    for train_image in train_images {
-        let distance = calculate_distance_between_images(image, &train_image);
-        if pq.len() < k as usize || *pq.peek().unwrap().1 > distance {
-            pq.pop();
-            pq.push(train_image.label, distance);
-        }
-    }
-
-    let closest_labels = Vec::new();
-    for (label, _) in pq.into_iter() {
-        closest_labels.push(label);
-    }
-    closest_labels
-}
-
 pub fn predict_image_category(k: u32, image: &Image, train_images: Vec<Image>) -> u8 {
     let closest_labels = find_closest_images(k, image, train_images);
 
@@ -34,4 +16,22 @@ pub fn predict_image_category(k: u32, image: &Image, train_images: Vec<Image>) -
         }
     }
     predicted_label
+}
+
+fn find_closest_images(k: u32, image: &Image, train_images: Vec<Image>) -> Vec<u8> {
+    let pq = PriorityQueue::with_capacity(k as usize);
+
+    for train_image in train_images {
+        let distance = calculate_distance_between_images(image, &train_image);
+        if pq.len() < k as usize || *pq.peek().unwrap().1 > distance {
+            pq.pop();
+            pq.push(train_image.label, distance);
+        }
+    }
+
+    let closest_labels = Vec::new();
+    for (label, _) in pq.into_iter() {
+        closest_labels.push(label);
+    }
+    closest_labels
 }
