75eb9836fcec1210ec4b976d0121c18b923a2dd5
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-10 01:20:52 -0400

    implement image category prediction

--- src/sequential.rs
+++ src/sequential.rs
@@ -20,5 +20,18 @@ pub fn find_closest_images(k: u32, image: &Image, train_images: Vec<Image>) -> V
 }
 
 pub fn predict_image_category(k: u32, image: &Image, train_images: Vec<Image>) -> u8 {
+    let closest_labels = find_closest_images(k, image, train_images);
 
+    let mut label_counts = [0; 10];
+    for label in closest_labels {
+        label_counts[label as usize] += 1;
+    }
+
+    let mut predicted_label = 0;
+    for (label, count) in label_counts.iter().enumerate() {
+        if *count > label_counts[predicted_label as usize] {
+            predicted_label = label as u8;
+        }
+    }
+    predicted_label
 }
