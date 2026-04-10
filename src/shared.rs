b91e0adf053b3c912259537abfe6671f999df771
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-09 21:33:25 -0400

    add constants to shared.rs

--- src/shared.rs
+++ src/shared.rs
@@ -1,6 +1,15 @@
 use priority_queue::PriorityQueue;
 use ndarray::{prelude::{Array3, Array2, s}};
 
+pub const MNIST_TRAINING_IMAGES: &str = "archive/train-images.idx3-ubyte";
+pub const MNIST_TRAINING_LABELS: &str = "archive/train-labels.idx1-ubyte";
+pub const MNIST_TEST_IMAGES: &str = "archive/t10k-images.idx3-ubyte";
+pub const MNIST_TEST_LABELS: &str = "archive/t10k-labels.idx1-ubyte";
+
+pub const TRAINING_SET_SIZE: usize = 60_000;
+pub const TEST_SET_SIZE: usize = 10_000;
+pub const IMAGE_SIDE_SIZE: usize = 28;
+
 pub struct Image {
     label: String,
     data: Array2<u8>,
@@ -23,11 +32,11 @@ pub fn predict_image_category(image: &Image) {
 }
 
 pub fn load_training_images(data: Vec<u8>) -> Array3<u8> {
-    Array3::from_shape_vec((60_000, 28, 28), data)
+    Array3::from_shape_vec((TRAINING_SET_SIZE, IMAGE_SIDE_SIZE, IMAGE_SIDE_SIZE), data)
         .expect("Error converting images to Array3 struct")
 }
 
 pub fn load_test_images(data: Vec<u8>) -> Array3<u8> {
-    Array3::from_shape_vec((10_000, 28, 28), data)
+    Array3::from_shape_vec((TEST_SET_SIZE, IMAGE_SIDE_SIZE, IMAGE_SIDE_SIZE), data)
         .expect("Error converting images to Array3 struct")
 }
