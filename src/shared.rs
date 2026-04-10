bb32ac488ce20f7d260479195f705a1b1be6d162
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-09 21:32:18 -0400

    make everything in shared public

--- src/shared.rs
+++ src/shared.rs
@@ -1,7 +1,7 @@
 use priority_queue::PriorityQueue;
 use ndarray::{prelude::{Array3, Array2, s}};
 
-struct Image {
+pub struct Image {
     name: String,
 }
 
@@ -9,15 +9,15 @@ pub fn calculate_distance_between_pixels(px1: u8, px2: u8) -> u8 {
     px1.abs_diff(px2)
 }
 
-fn calculate_distance_between_images(img1: &Image, img2: &Image) -> i8 {
+pub fn calculate_distance_between_images(img1: &Image, img2: &Image) -> u8 {
 
 }
 
-fn find_closest_images(k: i32, pq: &PriorityQueue) {
+pub fn find_closest_images(k: i32, pq: &PriorityQueue) {
 
 }
 
-fn predict_image_category(image: &Image) {
+pub fn predict_image_category(image: &Image) {
 
 }
 
