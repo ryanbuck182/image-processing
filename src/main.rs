d0be98a5620760cddaecce2d6c7a637c8ae1ad2d
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-10 01:44:08 -0400

    add function for testing sequential classification

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,21 @@ use shared::{TRAINING_SET_SIZE, load_dataset};
 use sequential::predict_image_category;
 
 fn main() {
-    let (train_images, test_images) = load_dataset();
-
-    let k = 3;
-    let test_image = &test_images[0];
-    let result = predict_image_category(k, test_image, train_images);
-
-    println!("Predicted: {}", result);
-    println!("Actual: {}", test_image.label);
+    test_sequential(3, 0);
 
     // for i in 0..TRAINING_SET_SIZE {
     //     println!("{:#?}\n", train_images[i].data);
     //     println!("Label: {:#?}", train_images[i].label);
     // }
+}
+
+fn test_sequential(k: usize, test_img_index: usize) {
+    let (train_images, test_images) = load_dataset();
+
+    let test_image = &test_images[test_img_index];
+    let result = predict_image_category(k, test_image, train_images);
+
+    println!("Predicted: {}", result);
+    println!("Actual: {}", test_image.label);
+    println!("{:#?}\n", test_image.data);
 }
