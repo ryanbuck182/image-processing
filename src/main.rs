96d51ca9b122684395e22145bedafead8dc652b2
ryanbuck182 <ryanbuck182@gmail.com>
2026-04-10 01:33:02 -0400

    add simple test to main

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@ mod parallel_1;
 mod parallel_2;
 
 use shared::{TRAINING_SET_SIZE, load_dataset};
+use sequential::predict_image_category;
 
 fn main() {
     let (train_images, test_images) = load_dataset();
 
-    for i in 0..TRAINING_SET_SIZE {
-        println!("{:#?}\n", train_images[i].data);
-        println!("Label: {:#?}", train_images[i].label);
-    }
+    let k = 3;
+    let test_image = &test_images[0];
+    let result = predict_image_category(k, test_image, train_images);
+
+    println!("Predicted: {}", result);
+    println!("Actual: {}", test_image.label);
+
+    // for i in 0..TRAINING_SET_SIZE {
+    //     println!("{:#?}\n", train_images[i].data);
+    //     println!("Label: {:#?}", train_images[i].label);
+    // }
 }
